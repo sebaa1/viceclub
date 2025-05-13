ae8ef7f0c55eba7683e60e14a045ea9187e46b37
seba <63276982+sebaa1@users.noreply.github.com>
2025-05-13 20:29:27 -0300

    Add smooth transition to menus (v. 0.4.1)

--- assets/scripts/abrir menu.js
+++ assets/scripts/abrir menu.js
@@ -1,18 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
   const hamburger = document.getElementById("hamburger");
   const menuContent = document.getElementById("menuContent");
-  const container = document.querySelector(".container"); // Seleccionamos el contenedor
-  
+  const container = document.querySelector(".container");
+
   hamburger.addEventListener("click", function (event) {
     event.stopPropagation();
     menuContent.classList.toggle("show");
-    container.classList.toggle("menu-shift"); // Desplazamos el contenedor
+    container.classList.toggle("menu-shift");
+    hamburger.classList.toggle("active");
   });
-  
+
   document.addEventListener("click", function (event) {
     if (!menuContent.contains(event.target) && !hamburger.contains(event.target)) {
       menuContent.classList.remove("show");
-      container.classList.remove("menu-shift"); // Volvemos al estado original
+      container.classList.remove("menu-shift");
+      hamburger.classList.remove("active");
     }
   });
 });
